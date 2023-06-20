99c426e30c80e09da149239f5f0e2be75c52d7dd
Bodhi Vandael <bodhi.vandael@ravago.com>
2023-06-20 20:39:43 +0200

    added nav items to hamburger

--- src/lib/components/ui/common/Hamburger.svelte
+++ src/lib/components/ui/common/Hamburger.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
+	import { gsap } from 'gsap';
+
 	let active = false;
+	let navWrapper: HTMLDivElement;
+	let tl = gsap.timeline();
+
+	const reverseTimeline = () => {
+		tl.reverse();
+		active = false;
+		document.body.style.overflow = 'auto';
+	};
 
 	const handleHamburgerClick = () => {
 		active = !active;
+		tl.reversed(!active);
 
 		if (active) {
+			tl.to(navWrapper, {
+				borderBottomLeftRadius: '0',
+				borderBottomRightRadius: '0',
+				height: '100vh',
+				duration: 0.5,
+				ease: 'power2.inOut'
+			}).to('.nav-items > a', {
+				opacity: 1,
+				transform: 'translateY(0)',
+				duration: 0.5,
+				ease: 'power3.inOut',
+				stagger: {
+					amount: 0.2
+				}
+			});
 			document.body.style.overflow = 'hidden';
 		} else {
+			tl.reverse();
 			document.body.style.overflow = 'auto';
 		}
 	};
@@ -14,20 +41,46 @@
 
 <button
 	on:click={handleHamburgerClick}
-	aria-label="hamburger menu"
+	aria-label="hamburger  menu"
 	class:active
-	class="hamburger block lg:hidden"
+	class="hamburger z-20 block lg:hidden"
 >
 	<div />
 	<div />
 	<div />
 </button>
 
+<div
+	bind:this={navWrapper}
+	class="fixed inset-0 z-10 flex h-0 items-center justify-center overflow-hidden rounded-b-[35%] bg-amber-100 dark:bg-slate-800"
+>
+	<div class="nav-items flex flex-col gap-20">
+		<a
+			on:click={reverseTimeline}
+			class="relative -translate-y-full text-6xl font-bold text-primary-dark opacity-0 dark:text-primary-light"
+			href="/blog"
+		>
+			<span class="absolute -translate-x-24 text-8xl opacity-20">01.</span>
+			Blog
+		</a>
+
+		<a
+			on:click={reverseTimeline}
+			class="relative -translate-y-full text-6xl font-bold text-primary-dark opacity-0 dark:text-primary-light"
+			href="/contact"
+		>
+			<span class="absolute -translate-x-24 text-8xl opacity-20">02.</span>
+			Contact
+		</a>
+	</div>
+</div>
+
 <style lang="scss">
 	.hamburger {
 		width: 30px;
 		height: 30px;
 		transition: 0.3s ease transform;
+		position: relative;
 	}
 
 	.hamburger div {
