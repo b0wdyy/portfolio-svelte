524ed45e87700418974f325f245a31417a77b1c4
Bodhi Vandael <bodhi.vandael@ravago.com>
2023-06-25 15:21:14 +0200

    chore: trying somethig out

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,13 @@
 	import '../app.scss';
 	import Nav from '$lib/components/ui/common/Nav.svelte';
 	import Footer from '$lib/components/ui/common/Footer.svelte';
+	import ogImage from '$lib/assets/thumbnail.jpeg';
 </script>
 
+<svelte:head>
+	<meta property="og:image" content={ogImage} />
+	<meta property="twitter:image" content={ogImage} />
+</svelte:head>
 <Nav />
 <slot />
 <Footer />
